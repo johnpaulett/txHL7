2439e81f7da13d986fe44d719252734f5cec7434
John Paulett <john@paulett.org>
2016-07-14 13:46:26 +0000

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='txHL7',
-    version='0.4.0',
+    version='0.4.1',
     author='John Paulett',
     author_email='john@paulett.org',
     description="""
